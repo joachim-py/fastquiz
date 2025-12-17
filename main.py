f2a6502a68777fb301571f833662227b4bd8227a
joachim-py <joachimigwebuike@proton.me>
2025-12-17 12:12:28 +0100

    CORS

--- main.py
+++ main.py
@@ -16,12 +16,15 @@ app = FastAPI(
 
 origins = [
     "http://localhost",
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
     "https://fastquiz-skmb.onrender.com",
+    "*",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
